f7f8a7ea2fe985f16f9e441e72ac8fe3f6f9e552
FatemaAliAhmed <f2a0t0k4@gmail.com>
2025-11-22 12:48:02 +0300

    Clear cookies on logout
    Added cookie clearing before session destruction.

--- logout.php
+++ logout.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+setcookie('user_id', '', time() - 3600, "/");
+setcookie('username', '', time() - 3600, "/");
+setcookie('role', '', time() - 3600, "/");
+
 session_destroy();
 header("Location: HomePage.php");
 exit();
